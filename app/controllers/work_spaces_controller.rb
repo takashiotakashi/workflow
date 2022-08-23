c985c097a27607c659e47e9cb06430cdc2c043a0
Mars Pinho <marciopinho@gmail.com>
2022-08-23 18:00:37 -0300

    links para spaces em index

--- app/controllers/work_spaces_controller.rb
+++ app/controllers/work_spaces_controller.rb
@@ -14,13 +14,11 @@ class WorkSpacesController < ApplicationController
 
   def create
     @work_space = WorkSpace.new(work_space_params)
-  
+
     if @work_space.save
       redirect_to work_space_path(@work_space), notice: "Work Space was successfully created."
     else
       # resolver se o new é com aspas ou simbolo
-      # mars: quase certo que é string, a ver.
-      # Esse new se refere à view de new para work_spaces.
       render :new, status: :unprocessable_entity
     end
   end
