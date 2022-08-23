cb47f628682b98cfae2a8b97ac406e3bb9fcdcb4
Mars Pinho <marciopinho@gmail.com>
2022-08-23 14:43:19 -0300

    mudanças feitas nas actions new e create de controller, validates do work_space, routes

--- app/controllers/work_spaces_controller.rb
+++ app/controllers/work_spaces_controller.rb
@@ -1,2 +1,22 @@
 class WorkSpacesController < ApplicationController
+
+  def new
+    @work_space = WorkSpace.new
+  end
+
+  def create
+    @work_space = WorkSpace.new(work_space_params)
+    # @work_space.save
+    if @work_space.save
+      redirect_to work_space_path(@work_space)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def work_space_params
+    params.require(:work_space).permit(:name, :description, :price)
+  end
 end
