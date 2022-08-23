6e2fffcb342a4d470086f52725a71256b7b18241
Mars Pinho <marciopinho@gmail.com>
2022-08-23 16:01:33 -0300

    retirei rotas redundantes em routes.rb

--- app/controllers/work_spaces_controller.rb
+++ app/controllers/work_spaces_controller.rb
@@ -7,7 +7,7 @@ class WorkSpacesController < ApplicationController
   def show
     @work_space = WorkSpace.find(params[:id])
   end
-  
+
   def new
     @work_space = WorkSpace.new
   end
@@ -17,9 +17,18 @@ class WorkSpacesController < ApplicationController
     if @work_space.save
       redirect_to work_space_path(@work_space), notice: "Work Space was successfully created."
     else
-      #resolver se o new é com aspas ou simbolo
-      render :new, status: :unprocessable_entity
+      # resolver se o new é com aspas ou simbolo
+      # mars: quase certo que é string, a ver.
+      # Esse new se refere à view de new para work_spaces.
+      render 'new', status: :unprocessable_entity
     end
+  end
+
+  def edit
+
+  end
+
+  def update
   end
 
   private
