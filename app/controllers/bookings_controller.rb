034d2b7f2134867316523304f733d652d54183ea
Carlos Esbeltte <carlosmechelli@gmail.com>
2022-08-24 11:47:47 -0300

    Finalizei pundit de bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,23 @@
 class BookingsController < ApplicationController
 
   def index
-    @bookings = Booking.all
+    @bookings = policy_scope(Booking)
   end
 
   def show
     @booking = Booking.find(params[:id])
+    authorize @booking
   end
 
   def new
     @booking = Booking.new
+    authorize @booking
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    authorize @booking
     if @booking.save
       redirect_to booking_path(@booking), notice: "Booking was successfully created."
     else
@@ -22,9 +26,30 @@ class BookingsController < ApplicationController
     end
   end
 
+  def edit
+    set_booking
+    authorize @booking
+  end
+
+  def update
+    authorize @booking
+    set_booking
+    @booking.update(booking_params)
+
+    if @booking.save
+      redirect_to booking_path(@booking), notice: "Booking was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :amount_due)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
   end
 end
